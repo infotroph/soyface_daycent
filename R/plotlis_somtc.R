1f7fc80e5711c90b723d81286f8277b184a54087
Chris Black <chris@bodger.org>
2015-10-19 15:14:54 -0500

    plot scripts lightly modified from CKB daycent versions

---
+++ R/plotlis_somtc.R
@@ -0,0 +1,51 @@
+#!/usr/bin/env Rscript
+
+library(ggplot2)
+library(grid)
+library(dplyr)
+library(ggplotTicks)
+library(DeLuciatoR)
+theme_set(theme_ggEHD(16))
+
+# If argv exists already, we're being sourced from inside another script.
+# If not, we're running standalone and taking arguments from the command line.
+if(!exists("argv")){
+	argv = commandArgs(trailingOnly = TRUE)
+}
+
+lis = read.csv(argv[1], colClasses=c("character", rep("numeric", 30)))
+lis = lis[lis$time >=1950,]
+lis$run = relevel(factor(lis$run), ref="ctrl")
+
+lis = (lis
+	%>% mutate(year=floor(time))
+	%>% group_by(run, year)
+	%>% select(somtc)
+	%>% summarise_each(funs(mean)))
+
+scale_labels = c(
+	ctrl="Control",
+	heat="Heat",
+	co2=expression(CO[2]),
+	heatco2=expression(paste("Heat+", CO[2])))
+scale_colors = c(ctrl="black", heat="black", co2="grey", heatco2="grey")
+scale_linetypes = c(ctrl=1, heat=2, co2=1, heatco2=2)
+
+plt = (ggplot(data=lis, aes(x=year, y=somtc, color=run, lty=run))
+	+geom_line()
+	+scale_color_manual(labels=scale_labels, values=scale_colors)
+	+scale_linetype_manual(labels=scale_labels, values=scale_linetypes)
+	+ylab(expression(paste("Daycent SOM, g C ", m^2)))
+	+theme(
+		legend.title=element_blank(),
+		legend.key=element_blank(),
+		legend.position=c(0.15,0.85),
+		legend.background=element_blank(),
+		legend.text.align=0))
+png_ggsized(
+	mirror.ticks(plt),
+	filename=paste0(argv[1], "_somtc.png"),
+	maxheight=9,
+	maxwidth=6.5,
+	units="in",
+	res=600)
--- R/plotresp.R
+++ R/plotresp.R
@@ -4,4 +4,143 @@ library(ggplot2)
 library(grid)
 library(gridExtra)
 library(DeLuciatoR) # https://github.com/infotroph/DeLuciatoR
-library(ggplotTicks) # https://github.com/infotroph/ggplotTickstheme_set(theme_ggEHD(16))
+library(ggplotTicks) # https://github.com/infotroph/ggplotTicks
+theme_set(theme_ggEHD(16))
+
+lm_eqn = function(mod){
+	eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(R)^2~"="~r2,
+		list(a = format(coef(mod)[1], digits = 2),
+			b = format(coef(mod)[2], digits = 2),
+			r2 = format(summary(mod)$r.squared, digits = 2)))
+	as.character(as.expression(eq))
+}
+
+resp = read.csv(
+	"../validation_data/soyface-2009to2011-soilresp.csv",
+	colClasses=c(
+		Effect="character",
+		Heat="factor",
+		CO2="factor",
+		Day="character",
+		Estimate="numeric",
+		Std.err="numeric",
+		DF="numeric",
+		t.val="numeric",
+		Pr.t="character",
+		Date="Date",
+		Part="factor",
+		Season="factor"))
+
+
+resp$Treatment = paste0(resp$Heat, resp$CO2)
+
+# If argv exists already, we're being sourced from inside another script.
+# If not, we're running standalone and taking arguments from the command line.
+if(!exists("argv")){
+	argv = commandArgs(trailingOnly = TRUE)
+}
+
+mresp = read.csv(paste0(argv[1], "_mresp.csv"), check.names=FALSE)
+gresp = read.csv(paste0(argv[1], "_gresp.csv"), check.names=FALSE)
+sysc = read.csv(paste0(argv[1], "_sysc.csv"), check.names=FALSE)
+
+scaleflux = function(x){
+	return(x
+	/12 # g C/mol
+	/86400 # sec/day
+	*1e6) # µmol/mol
+}
+
+dcresp = data.frame(
+	run=mresp$run,
+	time=mresp$time,
+	dayofyr=mresp$dayofyr,
+	mleaf=scaleflux(mresp$"cmrspflux(1)"),
+	mroot=scaleflux(mresp$"cmrspflux(2)" + mresp$"cmrspflux(3)"))
+ dcresp$gleaf = scaleflux(gresp$"cgrspflux(1)")
+ dcresp$groot = scaleflux(gresp$"cgrspflux(2)" + gresp$"cgrspflux(3)")
+ dcresp$hetresp = scaleflux(sysc$CO2resp)
+
+ rm(list=c("mresp", "gresp", "sysc"))
+
+dcresp$Date = as.Date(
+	paste(floor(dcresp$time), dcresp$dayofyr),
+	format="%Y %j")
+dcresp$Heat = factor(ifelse(
+		dcresp$run %in% c("heat", "heatco2"),
+		"h",
+		"c"))
+dcresp$CO2 = factor(ifelse(
+	dcresp$run %in% c("co2", "heatco2"),
+	"Elevated",
+	"Ambient"))
+
+# Why yes, I AM going from a four-level factor to two
+# two-level factors back to a new four-level factor.
+# Perhaps foolishly, I think this is the easiest way
+# to get matching Treatment names between simulated and observed datasets
+# without worrying about factor level ordering.
+dcresp$Treatment = paste0(dcresp$Heat, dcresp$CO2)
+
+dcresp = dcresp[dcresp$time >= 2009 & dcresp$time < 2012,]
+
+dcresp_aut = dcresp
+dcresp_aut$Part = "Raut"
+dcresp_aut$Estimate = dcresp$mroot + dcresp$groot
+
+dcresp_het = dcresp
+dcresp_het$Part = "Rhet"
+dcresp_het$Estimate = dcresp$hetresp
+
+dcresp_tot = dcresp
+dcresp_tot$Part = "Rtot"
+dcresp_tot$Estimate = dcresp$hetresp + dcresp$mroot + dcresp$groot
+
+dcresp_long=rbind(dcresp_aut, dcresp_het,dcresp_tot)
+
+partexpr = data.frame(
+	Part.txt=c("Rhet", "Raut", "Rtot"),
+	Part.expr=c("R[het]","R[aut]","R[tot]"))
+dcresp_long = merge(dcresp_long, partexpr, by.x="Part", by.y="Part.txt")
+resp = merge(resp, partexpr, by.x="Part", by.y="Part.txt")
+
+scale_labels = c(
+	cAmbient="Control",
+	hAmbient="Heat",
+	cElevated=expression(CO[2]),
+	hElevated=expression(paste("Heat+", CO[2])))
+
+pltl=(ggplot(data=dcresp_long,
+		aes(Date, Estimate, color=Treatment, shape=Treatment, lty=Treatment))
+	+facet_grid(Part.expr~., labeller=label_parsed)
+	+geom_line()
+	+geom_pointrange(data=resp,
+		aes(x=Date, y=Estimate, ymin=Estimate-Std.err, ymax=Estimate+Std.err),
+		alpha=0.95)
+	+scale_color_manual(
+		labels=scale_labels,
+		values=c(cAmbient="black", hAmbient="black", cElevated="grey", hElevated="grey"))
+	+scale_shape_manual(
+		labels=scale_labels,
+		values=c(cAmbient=1, hAmbient=2, cElevated=16, hElevated=17))
+	+scale_linetype_manual(
+		labels=scale_labels,
+		values=c(cAmbient=1, hAmbient=2, cElevated=1, hElevated=2))
+	+theme(
+		aspect.ratio=0.5,
+		legend.position=c(0.5,0.94),
+		legend.title=element_blank(),
+		legend.key=element_blank(),
+		legend.background=element_blank(),
+		legend.text.align=0)
+	+guides(col=guide_legend(ncol=2))
+	+labs(
+		y=expression(paste("Soil ", CO[2],  " efflux, µmol ", m^{-2}, " ", sec^{-1}))))
+
+png_ggsized(
+	mirror.ticks(pltl),
+	filename=paste(argv[1], "_resp_vs_dc.png", sep=""),
+	maxwidth=6.5,
+	maxheight=9,
+	units="in",
+	res=600)
